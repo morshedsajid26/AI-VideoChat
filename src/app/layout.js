cb66defaec30733ce2696e6aacd6fa33efea45be
neaz morshed sajid <neazmorshed407@gmail.com>
2025-11-08 12:08:12 +0600

    chnages

--- src/app/layout.js
+++ src/app/layout.js
@@ -40,7 +40,7 @@ export default function RootLayout({ children }) {
       >
         <ThemeProvider
         attribute="class"
-            defaultTheme="system"
+            defaultTheme="light"
             enableSystem
             disableTransitionOnChange
             
